0309bdf6ffed38672ae43b3bfd1c71d227418c7f
DAPotapov <103317598+DAPotapov@users.noreply.github.com>
2023-09-21 17:09:21 +0300

    Extract files

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # bad_memory_for_excel
 Helps when password to protected sheet had forgotten
 It is a training project for personal use. May not be used in any other way.
+
+TODO:  
+[] 1st aproach: extract all and zip all in the end
+[] 2nd approach: extract only needed and updated archive with changed files
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!.venv/bin/python
 
 import logging
+import os
 import sys
 import zipfile
 
@@ -22,8 +23,13 @@ def main():
         sys.exit("Provide MS Excel 2007 (or newer) file")
 
     print(sys.argv[1])
-    # with zipfile.ZipFile(sys.argv[1], "r") as file:
-
+    folder = os.path.split(sys.argv[1])[0]
+    filename = sys.argv[1].rsplit("/",1)[1]
+    temp_folder = os.path.join(folder, filename.rsplit(".",1)[0])
+    print(folder, filename, temp_folder)
+    with zipfile.ZipFile(sys.argv[1], "r") as file:
+        file.extractall(path=temp_folder)
+        # print(result)
     #     output_file = input_file.rsplit('.',1)[0] + '.csv'
 
 
