8543bc07d1ffafd512d44de31c65d6d260e93b44
DAPotapov <103317598+DAPotapov@users.noreply.github.com>
2023-09-21 14:11:13 +0300

    Environment initialization. Basic project structure

---
+++ app.py
@@ -0,0 +1,18 @@
+import logging
+import sys
+
+# Configure logging
+logging.basicConfig(filename=".data/logger.log", 
+                    filemode='a', 
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def main():
+
+    # Check for command-line usage
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python app.py <file to process>")
+
+if __name__ == '__main__':
+    main()
