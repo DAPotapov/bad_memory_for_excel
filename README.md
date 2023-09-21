aaa6346ed34d1d6c87f7950353dd6095f6d5d55a
DAPotapov <103317598+DAPotapov@users.noreply.github.com>
2023-09-21 18:26:57 +0300

    Get access to single sheet contents

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # bad_memory_for_excel
+
 Helps when password to protected sheet had forgotten
 It is a training project for personal use. May not be used in any other way.
 
--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 import logging
 import os
 import sys
+import xml.etree.ElementTree as ET
 import zipfile
 
 
@@ -25,13 +26,25 @@ def main():
     print(sys.argv[1])
     folder = os.path.split(sys.argv[1])[0]
     filename = sys.argv[1].rsplit("/",1)[1]
-    temp_folder = os.path.join(folder, filename.rsplit(".",1)[0])
+    temp_folder = os.path.join(folder, filename.rsplit(".", 1)[0])
     print(folder, filename, temp_folder)
     with zipfile.ZipFile(sys.argv[1], "r") as file:
         file.extractall(path=temp_folder)
-        # print(result)
-    #     output_file = input_file.rsplit('.',1)[0] + '.csv'
-
+        sheets_path = os.path.join(temp_folder, 'xl', 'worksheets')
+        if os.path.exists(sheets_path):
+            # print(os.listdir(sheets_path))
+            for sheet in os.listdir(sheets_path):
+                if os.path.isfile(os.path.join(sheets_path, sheet)):                    
+                    sheet_contents = ET.parse(os.path.join(sheets_path, sheet))
+                    root = sheet_contents.getroot()
+                    for child in root:
+                        print(child.tag, child.attrib)
+                    # for tag in root.items():
+                    #     print(tag)
+                    # tag = root.iterfind('sheetPr')
+                    # if tag:
+                    #     for x in tag:
+                    #         print(x.items())
 
 if __name__ == '__main__':
     main()
