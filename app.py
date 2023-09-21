baec71d2cb817e786e9eeee9bb69fa93e312f74a
DAPotapov <103317598+DAPotapov@users.noreply.github.com>
2023-09-21 20:34:27 +0300

    Some cleaning and commenting

--- app.py
+++ app.py
@@ -20,28 +20,30 @@ def main():
     if len(sys.argv) != 2:
         sys.exit("Usage: python app.py <file to process>")
 
+    # Check for filetype
     if not zipfile.is_zipfile(sys.argv[1]):
         sys.exit("Provide MS Excel 2007 (or newer) file")
 
-    print(sys.argv[1])
+    # Prepare folder structure and extract files from archive
     folder = os.path.split(sys.argv[1])[0]
     filename = sys.argv[1].rsplit("/",1)[1]
     temp_folder = os.path.join(folder, filename.rsplit(".", 1)[0])
-    print(folder, filename, temp_folder)
     with zipfile.ZipFile(sys.argv[1], "r") as file:
         file.extractall(path=temp_folder)
-        sheets_path = os.path.join(temp_folder, 'xl', 'worksheets')
-        if os.path.exists(sheets_path):
-            # print(os.listdir(sheets_path))
-            for sheet in os.listdir(sheets_path):
-                if os.path.isfile(os.path.join(sheets_path, sheet)):                    
-                    sheet_contents = ET.parse(os.path.join(sheets_path, sheet))
-                    root = sheet_contents.getroot()
-                    tag = root.find(".//{*}sheetProtection")
-                    if tag != None:
-                        print(tag.attrib)
-                        root.remove(tag)
-                        sheet_contents.write(os.path.join(sheets_path, sheet))
+
+    # Get into extracted file-tree and proceed with sheet's files
+    sheets_path = os.path.join(temp_folder, 'xl', 'worksheets')
+    if os.path.exists(sheets_path):
+        for sheet in os.listdir(sheets_path):
+            if os.path.isfile(os.path.join(sheets_path, sheet)):       
+
+                # Get content of sheet and search for needed tag, remove it if found             
+                sheet_contents = ET.parse(os.path.join(sheets_path, sheet))
+                root = sheet_contents.getroot()
+                tag = root.find(".//{*}sheetProtection")
+                if tag != None:
+                    root.remove(tag)
+                    sheet_contents.write(os.path.join(sheets_path, sheet))
 
 
 if __name__ == '__main__':
