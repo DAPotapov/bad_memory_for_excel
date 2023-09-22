98930ac1bfe65a21fdd130e543c72cbd1bf8cac2
DAPotapov <103317598+DAPotapov@users.noreply.github.com>
2023-09-22 09:39:38 +0300

    Compression (original - deflated) added. All files packed in a resulting file.

--- app.py
+++ app.py
@@ -24,11 +24,11 @@ def main():
     if not zipfile.is_zipfile(sys.argv[1]):
         sys.exit("Provide MS Excel 2007 (or newer) file")
 
-    # Prepare folder structure and extract files from archive
+    # Prepare folder structure and extract files from archive / dirname? / .isdir?
     folder = os.path.split(sys.argv[1])[0]
-    # TODO what if given file in current directory (no folder)?
+    # TODO what if given file in current directory (no folder)? / os.path.split / .isfile
     filename = sys.argv[1].rsplit("/",1)[1]
-    extension = filename.rsplit(".",1)[1]
+    extension = filename.rsplit(".",1)[1] # splitext
     temp_folder = os.path.join(folder, filename.rsplit(".", 1)[0])
     with zipfile.ZipFile(sys.argv[1], "r") as file:
         file.extractall(path=temp_folder)
@@ -50,10 +50,19 @@ def main():
     # Create new archive
     # TODO check if files were modified and inform user of made changes
     new_filename = filename.rsplit(".",1)[0] + '_updated.' + extension
-    with zipfile.ZipFile(os.path.join(folder, new_filename), "w") as new_file:
-        # TODO look here https://thispointer.com/python-how-to-create-a-zip-archive-from-multiple-files-or-directory/
-        for item in os.listdir(temp_folder):
-            new_file.write(os.path.join(temp_folder, item))
+    with zipfile.ZipFile(os.path.join(folder, new_filename), "w", compression=zipfile.ZIP_DEFLATED) as new_file:
+        
+        # Walk through subfolder and pack contents to archive relatively
+        for folder_name, subfolders, filenames in os.walk(temp_folder):
+            for filename in filenames:
+                # Path to file to pack
+                file_path = os.path.join(folder_name, filename)
+                # basename = os.path.basename(file_path)
+                
+                # Relative file path to store in archive
+                relpath = os.path.join(os.path.relpath(folder_name, temp_folder), filename)
+                # basename = os.path.join(relpath, filename)
+                new_file.write(file_path, relpath)
 
 
 if __name__ == '__main__':
